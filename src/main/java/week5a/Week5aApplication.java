5a8ae9a5cc675ddfc09ba4e01540e917e1e56250
Michael Hempenstall <michael@softworks.local>
2019-04-29 17:10:38 +0100

    Base Project Commit

---
+++ src/main/java/week5a/Week5aApplication.java
@@ -0,0 +1,13 @@
+package week5a;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week5aApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Week5aApplication.class, args);
+	}
+
+}
---
+++ src/main/java/week5a/controllers/ApplicationController.java
@@ -0,0 +1,24 @@
+package week5a.controllers;
+
+// Import framework dependencies - required to handle the HTTP request and send a response
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestParam;
+
+// The @ annotation identifies this as a Controller class
+@Controller
+public class ApplicationController {
+
+    // This method, index(), serves as the site index - the default page
+    // Requests for the root '/' will be handled by this method
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    @ResponseBody // Send a direct response without a view template
+    public String index() {
+        // Return some text (this will be the response back to the browser)
+        return "Hello World! This is the Home page";
+    }
+}
